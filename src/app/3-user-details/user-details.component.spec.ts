33effb6556ac249f05cc0a05664238d682e8f6f4
Javier Reyes <jreyes@portalfinance.co>
2019-04-10 11:47:16 -0500

    Testing with router object

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -4,14 +4,44 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { UserDetailsComponent } from './user-details.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/empty';
+import { Subject } from 'rxjs';
 
-xdescribe('UserDetailsComponent', () => {
+class RouterStub {
+  navigate(params) {}
+}
+
+class ActivatedRouterStub {
+  /* Se cambia el observable por un subject que se le puede inyectar data
+     se crea un método push para inyectarle al subject un valor
+     se crea un método get params para retornar el objecto subject como un Observable
+  */
+  private subject = new Subject();
+
+  push(value) {
+    this.subject.next(value);
+  }
+
+  get params() {
+    return this.subject.asObservable();
+  }
+
+  // params: Observable<any> = Observable.empty();
+}
+
+describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [ UserDetailsComponent ],
+      providers: [
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouterStub }
+      ]
     })
     .compileComponents();
   }));
@@ -24,5 +54,35 @@ xdescribe('UserDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Testing to navigate
+  it('should redirect the user to the users page after saving', () => {
+    // Arrange
+    const router = TestBed.get(Router); // Se asigna el router a la variable
+    const spy = spyOn(router, 'navigate'); // Spy el método navigate del Router
+
+    // Act (Lanza la función o el método)
+    component.save();
+
+    // Assert
+    expect(spy).toHaveBeenCalledWith(['users']); // Se le pasa el argumento de la ruta a la que deberia navegar
+
+  });
+
+  // Testing to navigate in ngOninit
+  it('should navigate the user to the not found page when an invalid user id is passed', () => {
+    // Arrange
+    const router = TestBed.get(Router); // Se asigna el router a la variable
+    const spy = spyOn(router, 'navigate'); // Spy el método navigate del Router
+
+    const route: ActivatedRouterStub = TestBed.get(ActivatedRoute);
+    route.push({ id: 0 });
+
+    // Act (Lanza la función o el método) No se lanza porque toma lo del ngOninit
+
+    // Assert
+    expect(spy).toHaveBeenCalledWith(['not-found']); // Se le pasa el argumento de la ruta a la que deberia navegar
+
   });
 });
